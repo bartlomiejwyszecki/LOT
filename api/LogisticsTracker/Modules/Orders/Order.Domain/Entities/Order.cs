1d6388660553a8de51a0c081af5783c2f3d371b6
unknown <bartlomiej.wyszecki2@gmail.com>
2025-12-03 23:07:38 +0100

    order entity

---
+++ api/LogisticsTracker/Modules/Orders/Order.Domain/Entities/Order.cs
@@ -0,0 +1,31 @@
+﻿namespace Order.Domain.Entities
+{
+    public class Order : Entity
+    {
+        public string OrderNumber { get; private set; }
+        public OrderStatus Status { get; private set; }
+        public Address ShippingAddress { get; private set; }
+        public DateTime OrderDate { get; private set; }
+
+        private Order() { }
+
+        private Order(string orderNumber, Address shippingAddress)
+        {
+            OrderNumber = orderNumber;
+            ShippingAddress = shippingAddress;
+            Status = OrderStatus.Pending;
+            OrderDate = DateTime.UtcNow;
+        }
+
+        public static Order Create(string orderNumber, Address shippingAddress)
+        {
+            return new Order(orderNumber, shippingAddress);
+        }
+
+        public void UpdateStatus(OrderStatus newStatus)
+        {
+            Status = newStatus;
+            UpdatedAt = DateTime.UtcNow;
+        }
+    }
+}
