d9b76957f3fc333daecc471b43b760d9b50938e0
unknown <bartlomiej.wyszecki2@gmail.com>
2026-01-06 01:13:02 +0100

    orders application createorderdto, adressdto

---
+++ api/LogisticsTracker/Modules/Orders/Orders.Application/DTOs/AddressDto.cs
@@ -0,0 +1,25 @@
+using Order.Domain.ValueObjects;
+
+namespace Orders.Application.DTOs;
+
+public record AddressDto
+{
+    public string Street { get; init; } = string.Empty;
+    public string City { get; init; } = string.Empty;
+    public string State { get; init; } = string.Empty;
+    public string PostalCode { get; init; } = string.Empty;
+    public string Country { get; init; } = string.Empty;
+
+    public static AddressDto FromValueObject(Address address)
+    {
+        return new AddressDto
+        {
+            Street = address.Street,
+            City = address.City,
+            State = address.State,
+            PostalCode = address.PostalCode,
+            Country = address.Country
+        };
+    }
+}
+
---
+++ api/LogisticsTracker/Modules/Orders/Orders.Application/DTOs/CreateOrderDto.cs
@@ -0,0 +1,12 @@
+namespace Orders.Application.DTOs;
+
+public record CreateOrderDto
+{
+    public string OrderNumber { get; init; } = string.Empty;
+    public string Street { get; init; } = string.Empty;
+    public string City { get; init; } = string.Empty;
+    public string State { get; init; } = string.Empty;
+    public string PostalCode { get; init; } = string.Empty;
+    public string Country { get; init; } = string.Empty;
+}
+
